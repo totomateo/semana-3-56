d1ccd4d4f29ecc34187e3e6f3b77c129e0466728
lmanaya <lmanaya990320@gmail.com>
2020-12-12 16:49:17 -0500

    Instalación de mysql, creación de la carpeta controllers, asignación de los campos de mysql, generación del modelo user, migración a la base de datos y asignar el primer valor

--- seeders/20201114225211-seed-user.js
+++ seeders/20201114225211-seed-user.js
@@ -2,17 +2,18 @@
 
 module.exports = {
     up: async(queryInterface, Sequelize) => {
-        return queryInterface.bulkInsert('Users', [{
+        return queryInterface.bulkInsert('users', [{
             name: 'carlos',
             email: 'ejemplo@gmail.com',
-            password: '$2y$08$FTP/jKGNASwJf0ero7SBe.kQmUsOSjWYupPZ6/lS6en6RcithXFKO',
+            password: '$2y$08$0Sn5Jqc.ZDC4xOWiuE7Yn.2mVG0L6Q8wb5JB2ry3Y05nssE6h9XGS', //micontraseña con 8 rounds
             createdAt: new Date(),
             updatedAt: new Date()
+            
         }]);
     },
 
     down: async(queryInterface, Sequelize) => {
-        return queryInterface.bulkDelete('Users', null, {});
+        return queryInterface.bulkDelete('users', null, {});
 
     }
 };
